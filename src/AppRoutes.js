74b4448f2e656177e85397293b02fa6d96f7e611
Brijeshjha671 <brijesh@binmile.com>
2022-03-24 16:15:13 +0530

    brijesh1commit

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -3,12 +3,15 @@ import "./App.css";
 import { Route,BrowserRouter as Router, Routes } from "react-router-dom";
 import Main from "./Main";
 
+import LoginForm from './components/login/LoginForm';
+
 function AppRoutes() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Main />}></Route>
+          <Route path='/' element={<Main />}></Route>
+          <Route path='/login' element={<LoginForm />}></Route>
         </Routes>
       </Router>
     </div>
