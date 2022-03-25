0bd1a60234850fd49a89931a11a68fca98e3e6b0
Mohitb1426 <rawatmohituk12@gmail.com>
2022-03-25 12:45:45 +0530

    keelearn component

---
+++ src/components/keepLearnKeepGrow/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const KeepLearnKeepGrow = () => {
+    return (
+        <div className="keepLearning-keepGrowing-div">
+            <h1>Keep learning. Keep growing</h1>
+        </div>
+    );
+}
+export default KeepLearnKeepGrow;
