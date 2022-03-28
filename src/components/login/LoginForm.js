d184bbe0652c8706ad5af649c341a879d94e469d
Mohitb1426 <rawatmohituk12@gmail.com>
2022-03-28 16:05:56 +0530

    completed validation and form layout

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,20 +1,33 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Modal from '@mui/material/Modal';
 import TextField from '@material-ui/core/TextField';
 import Button from '@mui/material/Button';
 import GoogleIcon from '@mui/icons-material/Google';
 import Grid from "@mui/material/Grid";
-// import Box from "@mui/material/Box";
 import './LoginForm.css';
+import swal from 'sweetalert';
 
 function LoginForm() {
+  const [Email, setEmail] = useState('')
+
+  const onContinue = () => {
+    const emailRegax =/\S+@\S+\.\S+/
+    if(Email !=='' && Email.match(emailRegax)){
+     
+     swal("Welcome");
+    }
+    else{
+      swal("Please Enter a Valid Email");
+      
+    }
+  }
   return (
     <React.Fragment>
       <Modal
         className="login-form-modal"
         hideBackdrop
         open={true}
-        //onClose={handleClose}
+
         aria-labelledby="child-modal-title"
         aria-describedby="child-modal-description"
       >
@@ -22,32 +35,41 @@ function LoginForm() {
           direction="row"
           alignItems="center"
           justifyContent="center"
-          style={{ minHeight: '50vh' }} columns={16}>
+          style={{ "minHeight": '50vh', "outline": "none" }} columns={16}>
           <Grid style={{ "height": "20em" }} item xs={8}>
             <div className="loginform-imagediv">
-              {/* <P className="login-form-left-title" >Simple ,Free <Br/>
-              Investing</P> */}
+              Simple, Free <br /> Investing.
+              <div className="loginform-imagediv-downside">US Stocks</div>
             </div>
+
           </Grid>
           <Grid item xs={8}>
-            <form   >
-              <h1 className='continueWithGoogle'>Welcome to Groww</h1>
-              <div>
-                <Button variant="outlined" startIcon={<GoogleIcon />} className='googlebutton'>Continue with Google</Button>
-              </div>
-              <div>
-                <TextField id="standard-basic" label="Your Email Address" variant="standard" />
-              </div>
-              <div>
-                <Button className='absolutecenter' style={{ padding: "25px 25px" }}>Continue</Button>
-              </div>
-              <div>
-                <p>By proceeding ,I agree to <span>T&C</span>and <span>Privacy Policy</span></p>
-              </div>
-            </form>
+            <div >
+              <form className='login-form' >
+                <h1 className='continueWithGoogle'>Welcome to Groww</h1>
+                <div >
+                  <Button variant="outlined" startIcon={<GoogleIcon />} className='googlebutton'>Continue with Google</Button>
+                </div>
+
+                <div className='loginForm-or-line'>Or</div>
+                <div>
+                  <TextField id="standard-basic"
+                    label="Your Email Address"
+                    variant="standard"
+                    className='loginForm-email-field'
+                    value={Email}
+                    onChange={e => setEmail(e.target.value)} />
+                </div>
+                <div>
+                  <Button onClick={onContinue} className='login-form-continue-button' >Continue</Button>
+                </div>
+                <div>
+                  <p className='login-form-privacypolicy-text'>By proceeding ,I agree to <span className='bold-policy-login'>T&C</span>and <span className='bold-policy-login'>Privacy Policy</span></p>
+                </div>
+              </form>
+            </div>
           </Grid>
         </Grid>
-
       </Modal>
     </React.Fragment>
   );
