e99e26bfc2714e244f55acc35baa23ea88b31615
Mohitb1426 <brijesh@binmile.com>
2022-03-29 18:18:50 +0530

    integrated images-brijesh

--- src/components/ourProduct/OurProduct.js
+++ src/components/ourProduct/OurProduct.js
@@ -8,6 +8,8 @@ import ShowChartIcon from '@mui/icons-material/ShowChart';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import ReorderIcon from '@mui/icons-material/Reorder';
+import Grid from "@mui/material/Grid";
+import ourProductsImage from "../../assets/images/ourProductImages/ourProductStocks.png";
 
 export default function OurProduct() {
 
@@ -16,37 +18,72 @@ export default function OurProduct() {
         setValue(newValue);
     };
     return (
-        <div className='box'>
-            <div className='top-2-heading'>
-                <Typography variant="body2" gutterBottom className='our-product-button'>
-                    OUR PRODUCTS
-                </Typography>
-                <Typography variant="h4" gutterBottom component="div" className='our-product-h4'>
-                    Your money. Your choice.
-                </Typography>
+        <div>
+            <div className='box'>
+                <div className='top-2-heading'>
+                    <Typography variant="body2" gutterBottom className='our-product-button'>
+                        OUR PRODUCTS
+                    </Typography>
+                    <Typography variant="h4" gutterBottom component="div" className='our-product-h4'>
+                        Your money. Your choice.
+                    </Typography>
+                </div>
+                <div>
+                    <Box sx={{
+                        width: '100%',
+                        float: "left",
+                        bgcolor: 'background.paper'
+                    }}>
+                        <Tabs value={value} onChange={handleChange} >
+                            <Tab className='item1' icon={<ShowChartIcon />} iconPosition="start" label="Stocks" />
+                            <Tab className='item2' icon={<ReorderIcon />} iconPosition="start" label="Mutual Funds" />
+                            <Tab className='item3' icon={<AttachMoneyIcon />} iconPosition="start" label="US Stocks" />
+                            <Tab className='item4' icon={<AccountBalanceIcon />} iconPosition="start" label="FDs" />
+                        </Tabs>
+                    </Box>
+                </div>
+                <div>
+                    <Box sx={{
+                        width: '100%',
+                        float: "left",
+                        bgcolor: 'background.paper'
+                    }}>
+                    </Box>
+                </div>
             </div>
             <div>
-                <Box sx={{
-                    width: '100%',
-                    float: "left",
-                    bgcolor: 'background.paper'
-                }}>
-                    <Tabs value={value} onChange={handleChange} >
-                        <Tab className='item1' icon={<ShowChartIcon />} iconPosition="start" label="Stocks" />
-                        <Tab className='item2' icon={<ReorderIcon />} iconPosition="start" label="Mutual Funds" />
-                        <Tab className='item3' icon={<AttachMoneyIcon />} iconPosition="start" label="US Stocks" />
-                        <Tab className='item4' icon={<AccountBalanceIcon />} iconPosition="start" label="FDs" />
-                    </Tabs>
-                </Box>
-            </div>
-            <div>
-                <Box sx={{
-                    width: '100%',
-                    float: "left",
-                    bgcolor: 'background.paper'
-                }}>
-                </Box>
-            </div>
-        </div>
-    );
+                <div className="keeplearn-grid">
+                    <Grid alignItems="flex-start" container spacing={1}>
+                        <Grid container direction="column" item xs={4} spacing={1}>
+                            <Grid item xs={12} >
+                                <Box
+                                    component="img"
+                                    sx={{
+                                        boxShadow: 1,
+                                        width: '20rem',
+                                        height: '16rem',
+                                        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : '#fff'),
+                                        color: (theme) =>
+                                            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+                                        borderRadius: 5,
+                                        textAlign: 'center',
+                                        fontSize: '0.875rem',
+                                        fontWeight: '700',
+                                        marginLeft:'200px',
+                                    }}
+                                    alt="The house from the offer."
+                                    src={ourProductsImage}
+                                />
+                            </Grid>    
+                        </Grid>
+                    </Grid>    
+                </div>
+                <div>
+                <Typography variant="h1" component="div" gutterBottom className='sidepart'>
+                   Zero
+                 </Typography>
+                </div>
+            </div> 
+        </div>       
+            );
 }
