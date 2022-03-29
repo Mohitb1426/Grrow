85ee6628c7a8d30169bbdee94071e9d006bff593
Mohitb1426 <brijesh@binmile.com>
2022-03-29 17:29:39 +0530

    working on ourProduct-brijesh

--- src/Main.js
+++ src/Main.js
@@ -3,14 +3,17 @@ import Header from "./commonComponent.js/header/Header";
 import HomeScreenCardViews from "./components/homeScreenComponent/HomeScreen";
 import KeepLearnKeepGrow from "./components/articlesComponent/Articles";
 import Investor from "./components/investorsComponent/Investors";
+import OurProduct from "./components/ourProduct/OurProduct";
 
 const Main = () => {
     return (
         <div>
             <Header />
             <HomeScreenCardViews />
+            <OurProduct/>
             <KeepLearnKeepGrow />
             <Investor />
+
         </div>
     );
 }
